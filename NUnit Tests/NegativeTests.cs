c975697931c4957deae2363670aae7332ed32313
Nahuel Rodriguez <nahuel.rodriguez@teaminternational.com>
2023-06-19 20:41:01 -0300

    second negative test added

--- NUnit Tests/NegativeTests.cs
+++ NUnit Tests/NegativeTests.cs
@@ -2,11 +2,6 @@
 using Minesweeper.Core.Enums;
 using MineSweeper.PositiveUnitTests;
 using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NUnit_Tests
 {
@@ -39,25 +34,23 @@ namespace NUnit_Tests
         }
 
         [Test]
-        [TestCase(5, 5, 9, 9, 0)] /* beginner open cell after win */
-        [TestCase(10, 10, 16, 16, 0)] /* intermediate open cell after win */
-        [TestCase(5, 5, 16, 30, 0)] /* expert open cell after win */
-        [TestCase(5, 5, 9, 9, 81)] /* beginner open cell after lose */
-        [TestCase(10, 10, 16, 16, 256)] /* intermediate open cell after lose */
-        [TestCase(5, 5, 16, 30, 480)] /* expert open cell after lose */
-        public void T02_RandomFieldGenerator_TryToOpenCellAfterBeingOpenedWithEverySizePosibleAfterWinAndLose_ThrowException(int x, int y, int row, int column, int mines)
+        [TestCase(10, 10, 9, 9, 0)] /* beginner open cell outside the grid */
+        [TestCase(17, 0, 16, 16, 0)] /* intermediate open cell outside the grid */
+        [TestCase(17, 0, 16, 30, 0)] /* expert open cell outside the grid */
+        /* verify error message when open cell outside the game grid */
+        public void T02_RandomFieldGenerator_OpenCellOutsideFieldWithEverySizePosible_ThrowErrorMessage(int x, int y, int row, int column, int mines)
         {
             /* precondition */
             _field = FieldGenerator.GetRandomField(row, column, mines);
             _gameProcessor = new GameProcessor(_field);
-            _gameProcessor.Open(x, y);
 
             /* action */
-            _gameProcessor.Open(x, y);
-
+            
             /* assert */
             Assert.Throws<InvalidOperationException>(() => _gameProcessor.Open(x, y));
         }
+
+
 
     }
 }
